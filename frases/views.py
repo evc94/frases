613eef248bae1b9a44c84c3fefd998b632571d7e
Estuardo <evc94mrc@gmail.com>
2015-11-05 17:56:53 -0600

    Plantillas

--- frases/views.py
+++ frases/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Frase
 
 def post_list(request):
-    return render(request, 'frases/post_list.html', {})
+    posts = Frase.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'frases/post_list.html', {'posts': posts})
