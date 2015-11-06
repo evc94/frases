7b57ce703d6d9a56839798e516483deb7e35a2d3
Estuardo <evc94mrc@gmail.com>
2015-11-06 00:30:22 -0600

    Detalle de frases implementadas

--- frases/views.py
+++ frases/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Frase
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    posts = Frase.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    posts = Frase.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
     return render(request, 'frases/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Frase, pk=pk)
+    return render(request, 'frases/post_detail.html', {'post': post})
